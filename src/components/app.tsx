519278de4e4fad45866fc3198247649612301698
Domenico Trematerra <d.trematerra@zakeke.com>
2026-01-21 10:55:49 +0100

    updated lib

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,25 +1,37 @@
-import React, { FunctionComponent } from 'react';
-import styled from 'styled-components';
-import { ZakekeEnvironment, ZakekeViewer, ZakekeProvider } from 'zakeke-configurator-react';
-import Selector from './selector';
+import React, { FunctionComponent } from "react";
+import styled from "styled-components";
+import {
+  ZakekeEnvironment,
+  ZakekeViewer,
+  ZakekeProvider,
+  useZakeke,
+} from "zakeke-configurator-react";
+import Selector from "./selector";
 
 const Layout = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-gap: 40px;
-    height: 100%;
-    padding: 40px;
-`
-
-const zakekeEnvironment = new ZakekeEnvironment();
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 40px;
+  height: 100%;
+  padding: 40px;
+`;
 
 const App: FunctionComponent<{}> = () => {
-    return <ZakekeProvider environment={zakekeEnvironment}>
-        <Layout>
-            <Selector />
-            <div><ZakekeViewer /></div>
-        </Layout>
-    </ZakekeProvider>;
-}
+  const { isSceneLoading } = useZakeke();
+  return (
+    <Layout>
+      {isSceneLoading ? (
+        <p>Loading scene...</p>
+      ) : (
+        <>
+          <Selector />
+          <div>
+            <ZakekeViewer bgColor="#f2f2f2" />
+          </div>
+        </>
+      )}
+    </Layout>
+  );
+};
 
-export default App; 
+export default App;
